e648aa1ee2736993ea77da1bf60abcb07379c3c7
grypesc <g.rypesc@wp.pl>
2022-12-06 20:17:59 +0100

    wup

--- src/approach/mvgb.py
+++ src/approach/mvgb.py
@@ -10,7 +10,22 @@ from argparse import ArgumentParser
 from torch import nn
 from torch.distributions import MultivariateNormal
 from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Compose
 from .incremental_learning import Inc_Learning_Appr
+
+
+class ClassDataset(torch.utils.data.Dataset):
+    """ Dataset consisting of samples of the same class """
+    def __init__(self, images, transforms):
+        self.images = images
+        self.transforms = transforms
+
+    def __len__(self):
+        return self.images.shape[0]
+
+    def __getitem__(self, index):
+        image = self.transforms(self.images[index].copy())
+        return image
 
 
 class DreamingDataset(torch.utils.data.Dataset):
@@ -26,6 +41,7 @@ class DreamingDataset(torch.utils.data.Dataset):
         target = random.randint(0, len(self.distributions)-1)
         val = self.distributions[target].sample(torch.Size([]))
         return val, target
+
 
 class WarmUpScheduler(nn.Module):
     """Warm-up and exponential decay chain scheduler. If warm_up_iters > 0 than warm-ups linearly for warm_up_iters iterations.
@@ -52,18 +68,19 @@ class Appr(Inc_Learning_Appr):
 
     def __init__(self, model, device, nepochs=100, lr=0.05, lr_min=1e-4, lr_factor=3, lr_patience=5, clipgrad=10000,
                  momentum=0, wd=0, multi_softmax=False, wu_nepochs=0, wu_lr_factor=1, fix_bn=False, eval_on_train=False,
-                 logger=None, use_multivariate=False, load_distributions=False):
+                 logger=None, use_multivariate=False, remove_outliers=False, load_distributions=False, save_distributions=False):
         super(Appr, self).__init__(model, device, nepochs, lr, lr_min, lr_factor, lr_patience, clipgrad, momentum, wd,
                                    multi_softmax, wu_nepochs, wu_lr_factor, fix_bn, eval_on_train, logger,
                                    exemplars_dataset=None)
         self.use_multivariate = use_multivariate
+        self.remove_outliers = remove_outliers
         self.load_distributions = load_distributions
+        self.save_distributions = save_distributions
+
         if load_distributions:
             with open(f"distributions.pickle", 'rb') as f:
                 data_file = pickle.load(f)
                 self.model.task_distributions = data_file["distributions"]
-                self.model.tasks_learned_so_far = len(self.model.task_distributions)
-                self.dream()
 
     @staticmethod
     def extra_parser(args):
@@ -71,6 +88,10 @@ class Appr(Inc_Learning_Appr):
         parser = ArgumentParser()
         parser.add_argument('--use-multivariate',
                             help='Use multivariate distribution',
+                            action='store_true',
+                            default=False)
+        parser.add_argument('--remove-outliers',
+                            help='Remove class outliers before creating distribution',
                             action='store_true',
                             default=False)
         parser.add_argument('--load-distributions',
@@ -88,14 +109,17 @@ class Appr(Inc_Learning_Appr):
         if self.load_distributions:
             return
 
-        # Loop epochs
+        # Train backbone
+        if t == 0:
+            self.train_backbone(t, trn_loader, val_loader)
 
-        for e in range(self.nepochs):
-            self.train_epoch(t, trn_loader, val_loader)
+        # Create distributions
+        self.create_distributions(t, trn_loader, val_loader)
 
-        # if not self.load_distributions:
-        #     with open(f"distributions.pickle", 'wb') as f:
-        #         pickle.dump({"distributions": self.model.task_distributions}, f)
+        # Dump distributions
+        if self.save_distributions:
+            with open(f"distributions.pickle", 'wb') as f:
+                pickle.dump({"distributions": self.model.task_distributions}, f)
 
     def dream(self):
         print("STARTING DREAMING")
@@ -125,54 +149,84 @@ class Appr(Inc_Learning_Appr):
 
         self.model.model.dreamer.eval()
 
-    def train_epoch(self, t, trn_loader, val_loader):
-        """Runs a single epoch"""
-        if self.load_distributions:
-            return
+    def train_backbone(self, t, trn_loader, val_loader):
+        return
+        model = self.model
+        optimizer, lr_scheduler = self._get_optimizer()
+        model.train()
+        for epoch in range(self.nepochs):
+            train_loss, valid_loss = [], []
+            for images, targets in trn_loader:
+                bsz = images.shape[0]
+                images, targets = images.to(self.device), targets.to(self.device)
+                optimizer.zero_grad()
+                out = model(images)
+                loss = self.criterion(t, out, targets)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), self.clipgrad)
+                optimizer.step()
+                lr_scheduler.step_iter()
+
+                train_loss.append(float(bsz * loss))
+            lr_scheduler.step_epoch()
+
+            with torch.no_grad():
+                for images, targets in val_loader:
+                    bsz = images.shape[0]
+                    images, targets = images.to(self.device), targets.to(self.device)
+                    out = model(images)
+                    loss = self.criterion(t, out, targets)
+
+                    valid_loss.append(float(bsz * loss))
+
+            train_loss = sum(train_loss) / len(train_loss)
+            val_loss = sum(valid_loss) / len(valid_loss)
+
+            print(f"Epoch: {epoch} Train loss: {train_loss:.2} Val loss: {valid_loss:.2}")
+
+    def create_distributions(self, t, trn_loader, val_loader):
+        """ Create distributions for task t"""
+        print("Creating distributions:")
         self.model.eval()
-        if not self.model.means[t].detach().all():
-            self.train_first_epoch(t, trn_loader, val_loader)
-            return
+        with torch.no_grad():
+            classes = self.model.taskcla[t][1]
+            self.model.task_offset.append(self.model.task_offset[-1] + classes)
+            transforms = Compose([t for t in val_loader.dataset.transform.transforms
+                                  if "ToTensor" in t.__class__.__name__
+                                  or "Normalize" in t.__class__.__name__])
+            for c in range(classes):
+                c = c + self.model.task_offset[t]
+                train_indices = torch.tensor(trn_loader.dataset.labels) == c
+                val_indices = torch.tensor(val_loader.dataset.labels) == c
+                ds = np.concatenate((trn_loader.dataset.images[train_indices], val_loader.dataset.images[val_indices]), axis=0)
+                ds = ClassDataset(ds, transforms)
+                loader = torch.utils.data.DataLoader(ds, batch_size=64, num_workers=0, shuffle=False)
+                from_ = 0
+                class_features = torch.full((2 * len(ds), 64), fill_value=-999999999.0, device=self.model.device)
+                for images in loader:
+                    bsz = images.shape[0]
+                    images = images.to(self.device)
+                    _, features = self.model(images, return_features=True)
+                    class_features[from_: from_+bsz] = features
+                    _, features = self.model(torch.flip(images, dims=(3,)), return_features=True)
+                    class_features[from_+bsz: from_+2*bsz] = features
+                    from_ += 2*bsz
 
-    def train_first_epoch(self, t, trn_loader, val_loader):
-        """ In the first epoch of a task t, calculate means and stds of selector outputs"""
-        selectors_output = torch.full((2*len(trn_loader.dataset) + 2*len(val_loader.dataset), self.model.selector_features_dim), fill_value=-999999999.0,
-                                      device=self.model.device)
+                if self.remove_outliers:
+                    median = torch.median(class_features, dim=0)[0]
+                    dist = torch.cdist(class_features, median.unsqueeze(0), p=2).squeeze(1)
+                    not_outliers = torch.topk(dist, int(0.99*class_features.shape[0]), largest=False, sorted=False)[1]
+                    class_features = class_features[not_outliers]
 
-        for i, (images, targets) in enumerate(trn_loader):
-            bsz = images.shape[0]
-            images = images.to(self.device)
-            features = self.model(images)
-            from_ = 2*i*trn_loader.batch_size
-            selectors_output[from_: from_+bsz] = features
-            features = self.model(torch.flip(images, dims=(3,)))
-            selectors_output[from_+bsz: from_+2*bsz] = features
+                # Calculate distribution
+                means = class_features.mean(dim=0)
+                if self.use_multivariate:
+                    covs = torch.cov(class_features.T)
+                else:
+                    covs = torch.diag(torch.std(class_features, dim=0))
 
-        for i, (images, targets) in enumerate(val_loader):
-            bsz = images.shape[0]
-            images = images.to(self.device)
-            features = self.model(images)
-            from_ = 2*len(trn_loader.dataset) + 2*i*val_loader.batch_size
-            selectors_output[from_: from_+bsz] = features
-            features = self.model(torch.flip(images, dims=(3,)))
-            selectors_output[from_+bsz: from_+2*bsz] = features
-
-        # Remove outliers
-        # median = torch.median(selectors_output, dim=0)[0]
-        # dist = torch.cdist(selectors_output, median.unsqueeze(0), p=2).squeeze(1)
-        # not_outliers = torch.topk(dist, int(0.99*selectors_output.shape[0]), largest=False, sorted=False)[1]
-        # selectors_output = selectors_output[not_outliers]
-
-        # Calculate distribution
-        self.model.means[t] = selectors_output.mean(dim=0)
-        if self.use_multivariate:
-            self.model.covs[t] = torch.cov(selectors_output.T)
-        else:
-            self.model.covs[t] = torch.diag(torch.std(selectors_output, dim=0))
-
-        # self.model.covs[t] += torch.diag(torch.full((self.model.selector_features_dim,), fill_value=1e-4, device=self.model.device))
-        self.model.task_distributions.append(MultivariateNormal(self.model.means[t], self.model.covs[t]))
-        self.model.tasks_learned_so_far = t+1
+                # covs += torch.diag(torch.full((covs.shape[0],), fill_value=10, device=covs.device))
+                self.model.task_distributions.append(MultivariateNormal(means, covs))
 
     def eval(self, t, val_loader):
         """Contains the evaluation code"""
@@ -182,7 +236,7 @@ class Appr(Inc_Learning_Appr):
             for images, targets in val_loader:
                 targets = targets.to(self.device)
                 # Forward current model
-                features = self.model(images.to(self.device))
+                _, features = self.model(images.to(self.device), return_features=True)
                 hits_taw, hits_tag = self.calculate_metrics(features, targets, t)
                 # Log
                 total_loss = 0
@@ -193,7 +247,7 @@ class Appr(Inc_Learning_Appr):
 
     def criterion(self, t, outputs, targets):
         """Returns the loss value"""
-        return torch.nn.functional.cross_entropy(outputs[t], targets - self.model.task_offset[t])
+        return nn.functional.cross_entropy(outputs, targets, label_smoothing=0.1)
 
     def calculate_metrics(self, features, targets, t):
         """Contains the main Task-Aware and Task-Agnostic metrics"""
@@ -215,6 +269,7 @@ class Appr(Inc_Learning_Appr):
 
     def _get_optimizer(self):
         """Returns the optimizer"""
-        return torch.optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.wd)
-
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.wd)
+        scheduler = WarmUpScheduler(optimizer, 100, 0.95)
+        return optimizer, scheduler
 
