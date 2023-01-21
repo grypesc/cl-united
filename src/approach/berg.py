41e9cfac4acd654207f3371e3b6b2c25d57aafe6
grypesc <g.rypesc@wp.pl>
2023-01-21 10:55:52 +0100

    saving models in exp dir

--- src/approach/berg.py
+++ src/approach/berg.py
@@ -183,7 +183,7 @@ class Appr(Inc_Learning_Appr):
                   f"Train acc: {100 * train_acc:.2f} Val acc: {100 * val_acc:.2f}")
         model.fc = nn.Identity()
         self.model.bbs[t] = model
-        torch.save(self.model.state_dict(), "best_{}_{}.pth".format(self.model.network_type, len(self.model.taskcla)))
+        torch.save(self.model.state_dict(), f"{self.logger.exp_path}/model.pth")
 
     @torch.no_grad()
     def _choose_backbone_to_finetune(self, t, trn_loader, val_loader):
@@ -305,7 +305,7 @@ class Appr(Inc_Learning_Appr):
 
         model.fc = nn.Identity()
         self.model.bbs[bb_to_finetune] = model
-        torch.save(self.model.state_dict(), "best_{}_{}_ft.pth".format(self.model.network_type, len(self.model.taskcla)))
+        torch.save(self.model.state_dict(), f"{self.logger.exp_path}/model_ft.pth")
         return old_model
 
     def _drift_compensation(self, bb_finetuned, trn_loader, val_loader, old_model):
