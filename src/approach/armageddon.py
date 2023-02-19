d74bf0a10e61a670e0d3265afbd43285a0c1cdc4
grypesc <g.rypesc@wp.pl>
2023-02-19 21:32:26 +0100

    wip

--- src/approach/armageddon.py
+++ src/approach/armageddon.py
@@ -1,5 +1,6 @@
 import copy
 import random
+from itertools import compress
 
 import numpy as np
 import torch
@@ -8,54 +9,75 @@ from argparse import ArgumentParser
 from torch import nn
 
 from .incremental_learning import Inc_Learning_Appr
+from src.networks.resnet32 import resnet32
+from .mvgb import ClassDirectoryDataset, ClassMemoryDataset
 
 torch.backends.cuda.matmul.allow_tf32 = False
 
 
-class EncoderBlock(nn.Module):
-    def __init__(self):
-        super().__init__()
-        planes = 32
-        self.layers = nn.Sequential(nn.Conv2d(3, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(planes, 2*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(2*planes, 4*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.MaxPool2d((2, 2)),
-                                    nn.Conv2d(4*planes, 8*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(8*planes, 4*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.MaxPool2d((2, 2)),
-                                    nn.Conv2d(4*planes, 2*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(2*planes, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(planes, 8, kernel_size=3, stride=1, padding=1)
-                                    )# 8x8x8
+class MembeddingDataset(torch.utils.data.Dataset):
+    def __init__(self, membeddings_per_class: int):
+        self.labels = torch.zeros((0,), dtype=torch.int64)
+        self.data = torch.zeros((0, 512), dtype=torch.float)
+        self.membeddings_per_class = membeddings_per_class
 
-    def forward(self, x):
-        x = self.layers(x)
-        return x
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
+
+    def add(self, label, new_data):
+        new_labels = label.expand(new_data.shape[0])
+        self.labels = torch.cat((self.labels, new_labels), dim=0)
+        self.data = torch.cat((self.data, new_data), dim=0)
 
 
-class DecoderBlock(nn.Module):
-    def __init__(self):
-        super().__init__()
-        planes = 32
-        self.layers = nn.Sequential(nn.Conv2d(8, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(planes, 2*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.UpsamplingBilinear2d(scale_factor=2),
-                                    nn.Conv2d(2*planes, 4*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(4*planes, 8*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.UpsamplingBilinear2d(scale_factor=2),
-                                    nn.Conv2d(8*planes, 4*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(4*planes, 2*planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(2*planes, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
-                                    nn.Conv2d(planes, 3, kernel_size=3, stride=1, padding=1)
-                                    )
+class SlowLearner(nn.Module):
 
-    def forward(self, x):
-        x = self.layers(x)
-        return x
+    class EncoderBlock(nn.Module):
+        def __init__(self):
+            super().__init__()
+            planes = 32
+            self.layers = nn.Sequential(nn.Conv2d(3, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                        nn.Conv2d(planes, 2 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                        nn.Conv2d(2 * planes, 4 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                        nn.MaxPool2d((2, 2)),
+                                        nn.Conv2d(4 * planes, 8 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                        nn.Conv2d(8 * planes, 4 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                        nn.MaxPool2d((2, 2)),
+                                        nn.Conv2d(4 * planes, 2 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                        nn.Conv2d(2 * planes, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                        nn.Conv2d(planes, 8, kernel_size=3, stride=1, padding=1)
+                                        )
 
+        def forward(self, x):
+            x = self.layers(x)
+            return x
 
-class VisualCortex(nn.Module):
-    """https://github.com/Puayny/Autoencoder-image-similarity/blob/master/Autoencoder%2C%20cifar-100%20dataset.ipynb"""
+    class DecoderBlock(nn.Module):
+        def __init__(self):
+            super().__init__()
+            planes = 32
+            self.layers1 = nn.Sequential(nn.Conv2d(8, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                         nn.Conv2d(planes, 2 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                         nn.UpsamplingBilinear2d(scale_factor=2),
+                                         nn.Conv2d(2 * planes, 4 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                         nn.Conv2d(4 * planes, 8 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                         nn.UpsamplingBilinear2d(scale_factor=2)
+                                         )
+
+            self.layers2 = nn.Sequential(nn.Conv2d(8 * planes, 4 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                         nn.Conv2d(4 * planes, 2 * planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                         nn.Conv2d(2 * planes, planes, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(),
+                                         nn.Conv2d(planes, 3, kernel_size=3, stride=1, padding=1)
+                                         )
+
+        def forward(self, z):
+            x = z.reshape(z.shape[0], 8, 8, 8)
+            feature_maps = self.layers1(x)
+            return feature_maps, self.layers2(feature_maps)
+
     def __init__(self, z_size, planes=(3, 64, 128, 256, 512)):
         super().__init__()
         self.z_size = z_size
@@ -63,17 +85,16 @@ class VisualCortex(nn.Module):
 
         self.linear1 = nn.Conv2d(planes[0], planes[1], kernel_size=3, stride=2, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes[1])
-        self.encoder = EncoderBlock()
-        self.decoder = DecoderBlock()
+        self.encoder = SlowLearner.EncoderBlock()
+        self.decoder = SlowLearner.DecoderBlock()
 
     def forward(self, x, decode=True):
         x = self.encoder(x)
         z = x.reshape(x.shape[0], -1)
         if not decode:
-            return z, None
-        x = z.reshape(x.shape[0], 8, 8, 8)
-        x = self.decoder(x)
-        return z, x
+            return z
+        feature_maps, x = self.decoder(x)
+        return z, feature_maps, x
 
     def visualize(self, out, target):
         from PIL import Image
@@ -94,8 +115,8 @@ class VisualCortex(nn.Module):
 class MLP(nn.Module):
     def __init__(self, z_size, hidden_size, out_size):
         super().__init__()
-        self.linear1 = nn.Linear(z_size, 2 * hidden_size)
-        self.linear2 = nn.Linear(2 * hidden_size, hidden_size)
+        self.linear1 = nn.Linear(z_size, 4 * hidden_size)
+        self.linear2 = nn.Linear(4 * hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, out_size)
         self.activation = nn.LeakyReLU()
 
@@ -106,57 +127,76 @@ class MLP(nn.Module):
 
 
 class Appr(Inc_Learning_Appr):
-    """Class implementing the joint baseline"""
+    """https://www.youtube.com/watch?v=wfa9xH3cJ8E"""
 
     def __init__(self, model, device, nepochs=200, lr=0.05, lr_min=1e-4, lr_factor=3, lr_patience=5, clipgrad=10000,
                  momentum=0, wd=0, multi_softmax=False, wu_nepochs=0, wu_lr_factor=1, fix_bn=False, eval_on_train=False,
-                 logger=None, tau=3.0):
+                 logger=None, membeddings=100):
         super(Appr, self).__init__(model, device, nepochs, lr, lr_min, lr_factor, lr_patience, clipgrad, momentum, wd,
                                    multi_softmax, wu_nepochs, wu_lr_factor, fix_bn, eval_on_train, logger,
                                    exemplars_dataset=None)
         self.task_offset = [0]
         self.model = None
-        self.tau = tau
+        self.membeddings_per_class = membeddings
+        self.membeddings = MembeddingDataset(self.membeddings_per_class)
 
-        self.cortex = VisualCortex(512)
-        self.cortex.to(device)
-        self.classifier = MLP(512, 1024, 10)
-        self.classifier.to(device)
+        self.slow_learner = SlowLearner(512)
+        self.slow_learner.to(device)
+        self.fast_learner = resnet32()
         self.criterion = torch.nn.CrossEntropyLoss()
 
     @staticmethod
     def extra_parser(args):
         """Returns a parser containing the approach specific parameters"""
         parser = ArgumentParser()
-        parser.add_argument('--tau',
-                            help='gumbel softmax temperature',
-                            type=float,
-                            default=3.0)
+        parser.add_argument('--membeddings',
+                            help='number of embeddings per class',
+                            type=int,
+                            default=100)
 
         return parser.parse_known_args(args)
 
     def train_loop(self, t, trn_loader, val_loader):
-        if t == 0:
-            print(f"Training visual cortex")
-            self.train_cortex(t, trn_loader, val_loader)
-            # state_dict = torch.load("cortex_best.pth")
-            # self.cortex.load_state_dict(state_dict, strict=True)
-        self.train_classifier(t, trn_loader, val_loader)
+        print(f"Training slow learner on task {t}")
+        self.train_slow_learner(t, trn_loader, val_loader)
+        # state_dict = torch.load("slow_learner_10.pth")
+        # self.slow_learner.load_state_dict(state_dict, strict=True)
+        self.store_membeddings(t, trn_loader, val_loader.dataset.transform)
+        print(f"Training classifier")
+        self.train_fast_learner(t, trn_loader, val_loader)
 
-    def train_cortex(self, t, trn_loader, val_loader):
-        model = self.cortex
+    def train_slow_learner(self, t, trn_loader, val_loader):
+        model = self.slow_learner
+        old_model = copy.deepcopy(self.slow_learner)
+        old_model.eval()
         print(f'Cortex has {sum(p.numel() for p in model.parameters() if p.requires_grad):,} trainable parameters')
         model.to(self.device)
-        optimizer, lr_scheduler = self._get_optimizer(model, self.wd, milestones=[50, 80, 150, 190])
+        old_model.to(self.device)
+        if len(self.membeddings) > 0:
+            mem_loader = torch.utils.data.DataLoader(self.membeddings, batch_size=trn_loader.batch_size, num_workers=trn_loader.num_workers, shuffle=True)
+            mem_loader_iter = iter(mem_loader)
+
+        optimizer, lr_scheduler = self._get_optimizer(model, self.wd, milestones=[50, 100, 150, 180])
         for epoch in range(self.nepochs):
             train_loss, valid_loss = [], []
             model.train()
             for images, _ in trn_loader:
+
                 bsz = images.shape[0]
                 images = images.to(self.device)
                 optimizer.zero_grad()
-                z, reconstructed = model(images)
+                _, _, reconstructed = model(images)
                 loss = nn.functional.mse_loss(reconstructed, images)
+
+                if len(self.membeddings) > 0:
+                    membeddings = next(mem_loader_iter)[0].to(self.device)
+                    mem_loader_iter = iter(mem_loader)
+                    with torch.no_grad():
+                        mem_target = old_model.decoder(membeddings)[1]
+                    _, _, mem_reconstructed = model(mem_target)
+                    mem_loss = nn.functional.mse_loss(mem_reconstructed, mem_target)
+                    loss = loss + mem_loss
+
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), self.clipgrad)
                 optimizer.step()
@@ -168,36 +208,46 @@ class Appr(Inc_Learning_Appr):
                 for images, _ in val_loader:
                     bsz = images.shape[0]
                     images = images.to(self.device)
-                    z, reconstructed = model(images)
+                    z, _, reconstructed = model(images)
                     loss = nn.functional.mse_loss(reconstructed, images)
+
+                    if len(self.membeddings) > 0:
+                        membeddings = next(mem_loader_iter)[0].to(self.device)
+                        mem_loader_iter = iter(mem_loader)
+                        mem_target = old_model.decoder(membeddings)[1]
+                        _, _, mem_reconstructed = model(mem_target)
+                        mem_loss = nn.functional.mse_loss(mem_reconstructed, mem_target)
+                        loss = loss + mem_loss
+
                     valid_loss.append(float(bsz * loss))
 
             train_loss = sum(train_loss) / len(trn_loader.dataset)
             valid_loss = sum(valid_loss) / len(val_loader.dataset)
 
             print(f"Epoch: {epoch} Train loss: {train_loss:.2f} Val loss: {valid_loss:.2f}")
-        self.cortex = model
-        torch.save(self.cortex.state_dict(), f"cortex.pth")
+        self.slow_learner = model
+        torch.save(self.slow_learner.state_dict(), f"slow_learner.pth")
 
-    def train_classifier(self, t, trn_loader, val_loader):
-        model = self.classifier
+    def train_fast_learner(self, t, trn_loader, val_loader):
+        model = self.fast_learner
+        model.fc = nn.Linear(64, self.task_offset[t+1])
+        model.to(self.device)
         print(f'Classifier has {sum(p.numel() for p in model.parameters() if p.requires_grad):,} trainable parameters')
 
-        model.to(self.device)
-        optimizer, lr_scheduler = self._get_optimizer(model, self.wd, milestones=[40, 80])
+        self.slow_learner.eval()
+        mem_loader = torch.utils.data.DataLoader(self.membeddings, batch_size=trn_loader.batch_size, num_workers=trn_loader.num_workers, shuffle=True)
+        optimizer, lr_scheduler = self._get_optimizer(model, self.wd, milestones=[50, 100, 150])
         for epoch in range(self.nepochs):
             train_loss, valid_loss = [], []
             train_hits, val_hits = 0, 0
             model.train()
-            for images, targets in trn_loader:
-                targets -= self.task_offset[t]
-                bsz = images.shape[0]
-                images, targets = images.to(self.device), targets.to(self.device)
+            for membeddings, targets in mem_loader:
+                bsz = membeddings.shape[0]
+                membeddings, targets = membeddings.to(self.device), targets.to(self.device)
                 optimizer.zero_grad()
                 with torch.no_grad():
-                    embeddings, _ = self.cortex(images, decode=True)
-                    # self.cortex.visualize(_, images)
-                out = model(embeddings)
+                    feature_maps = self.slow_learner.decoder(membeddings)[0]
+                out = model(feature_maps)
                 loss = self.criterion(out, targets)
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), self.clipgrad)
@@ -212,11 +262,9 @@ class Appr(Inc_Learning_Appr):
                     targets -= self.task_offset[t]
                     bsz = images.shape[0]
                     images, targets = images.to(self.device), targets.to(self.device)
-                    with torch.no_grad():
-                        embeddings, _ = self.cortex(images, decode=False)
-                    out = model(embeddings)
+                    _, feature_maps, _ = self.slow_learner(images, decode=True)
+                    out = model(feature_maps)
                     loss = self.criterion(out, targets)
-
                     val_hits += float(torch.sum((torch.argmax(out, dim=1) == targets)))
                     valid_loss.append(float(bsz * loss))
 
@@ -227,20 +275,44 @@ class Appr(Inc_Learning_Appr):
 
             print(f"Epoch: {epoch} Train loss: {train_loss:.2f} Val loss: {valid_loss:.2f} "
                   f"Train acc: {100 * train_acc:.2f} Val acc: {100 * val_acc:.2f}")
-        model.fc = nn.Identity()
-        self.model.bbs[t] = model
-        torch.save(self.model.state_dict(), f"{self.logger.exp_path}/model.pth")
+        self.fast_learner = model
+
+    @torch.no_grad()
+    def store_membeddings(self, t, trn_loader, transforms):
+        labels = np.array(trn_loader.dataset.labels)
+        classes_ = set(trn_loader.dataset.labels)
+        self.task_offset += [len(classes_) + self.task_offset[t]]
+        for i in classes_:
+            class_indices = labels == i
+            if isinstance(trn_loader.dataset.images, list):
+                train_images = list(compress(trn_loader.dataset.images, class_indices))
+                train_images = train_images[:self.membeddings_per_class]
+                ds = ClassDirectoryDataset(train_images, transforms)
+            else:
+                ds = trn_loader.dataset.images[class_indices][:self.membeddings_per_class]
+                ds = ClassMemoryDataset(ds, transforms)
+            loader = torch.utils.data.DataLoader(ds, batch_size=self.membeddings_per_class, num_workers=0, shuffle=True)
+            for images in loader:
+                images = images.to(self.device)
+                membeddings = self.slow_learner(images, decode=False)
+                membeddings = membeddings.cpu()
+                self.membeddings.add(torch.tensor(i), membeddings)
 
     @torch.no_grad()
     def eval(self, t, val_loader):
         """Contains the evaluation code"""
         total_loss, total_acc_taw, total_acc_tag, total_num = 0, 0, 0, 0
-        self.model.eval()
+        self.slow_learner.eval()
+        self.fast_learner.eval()
         for images, targets in val_loader:
             targets = targets.to(self.device)
             # Forward current model
-            features = self.model(images.to(self.device))
-            hits_taw, hits_tag = self.calculate_metrics(features, targets, t)
+            _, feature_maps, _ = self.slow_learner(images.to(self.device), decode=True)
+            logits = self.fast_learner(feature_maps)
+            preds = torch.argmax(logits, dim=1)
+            hits_tag = preds == targets
+            preds = torch.argmax(logits[:, self.task_offset[t]:self.task_offset[t+1]], dim=1) + self.task_offset[t]
+            hits_taw = preds == targets
             # Log
             total_loss = 0
             total_acc_taw += hits_taw.sum().item()
@@ -258,6 +330,6 @@ class Appr(Inc_Learning_Appr):
 
     def _get_optimizer(self, model, wd, milestones=[60, 120, 160]):
         """Returns the optimizer"""
-        optimizer = torch.optim.AdamW(model.parameters(), lr=self.lr)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=self.lr, weight_decay=wd)
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=milestones, gamma=0.1)
         return optimizer, scheduler
