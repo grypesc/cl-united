dfb397dd0de4faefd1ae9bdb9ac833d9bdb823d7
grypesc <g.rypesc@wp.pl>
2024-01-14 23:52:28 +0100

    w

--- scripts/painkiller_ce.sh
+++ scripts/painkiller_ce.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for SEED in 0
 do
-  for S in 16 32
+  for ALPHA in 0
   do
-    python src/main_incremental.py --approach painkiller --batch-size 128 --num-workers 4 --nepochs 100 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.05 --weight-decay 5e-4 --adapt --S $S --use-test-as-val --extra-aug fetril --criterion ce --seed $SEED --nnet resnet32 --exp-name vanilla/ce_resnet32_S=${S}
+    python src/main_incremental.py --approach painkiller --batch-size 128 --num-workers 4 --nepochs 100 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.05 --weight-decay 1e-5 --adapt --use-test-as-val --alpha $ALPHA --criterion ce --adapter mlp --seed $SEED --nnet resnet32 --exp-name joint_classifier/ce_mlp_alpha=${ALPHA}
   done
 done
