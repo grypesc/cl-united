4b251292a95193528557f33e4349ef3c7478ddff
grypesc <g.rypesc@wp.pl>
2024-02-04 22:12:37 +0100

    lol

--- scripts/babel.sh
+++ scripts/babel.sh
@@ -3,6 +3,6 @@ for SEED in 0
 do
   for ALPHA in 0.5
   do
-    python src/main_incremental.py --approach soil --batch-size 128 --num-workers 4 --nepochs 100 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.1 --weight-decay 5e-4 --adapt --S 64 --N 5 --alpha $ALPHA --use-test-as-val --criterion ce --seed $SEED --distiller linear --nnet resnet32 --exp-name v1/linear_alpha=${ALPHA}
+    python src/main_incremental.py --approach babel --batch-size 128 --num-workers 4 --nepochs 100 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.1 --weight-decay 5e-4 --adapt --S 64 --N 5 --alpha $ALPHA --use-test-as-val --criterion ce --seed $SEED --distiller linear --nnet resnet32 --exp-name v1/linear_alpha=${ALPHA}
   done
 done
