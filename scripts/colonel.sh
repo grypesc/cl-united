338ebd0c453262a257769ec1b8388d2ddbdf623d
grypesc <g.rypesc@wp.pl>
2024-02-28 00:01:58 +0100

    colo

--- scripts/colonel.sh
+++ scripts/colonel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for SEED in 0
 do
-  for ALPHA in 0.5 0.9 0.99 0.0
+  for BETA in 5 15
   do
-    python src/main_incremental.py --approach colonel --batch-size 128 --num-workers 4 --nepochs 100 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.1 --weight-decay 5e-4 --adapt --S 64 --N 100 --alpha $ALPHA --use-test-as-val --seed $SEED  --K 21 --head linear --distiller linear --nnet resnet32 --exp-name colonel2/linear_alpha=${ALPHA}
+    CUDA_VISIBLE_DEVICES=0 python src/main_incremental.py --approach colonel --batch-size 128 --num-workers 4 --nepochs 200 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.1 --weight-decay 5e-4 --adapt --S 64 --N 100 --alpha 10 --beta $BETA --use-test-as-val --seed $SEED  --K 21 --distiller linear --nnet resnet32 --exp-name colo/linear_beta=${BETA}
   done
 done
