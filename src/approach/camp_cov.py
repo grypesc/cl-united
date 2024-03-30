rotation fix for singularity loss

src/approach/camp_cov.py
@@ -83,7 +83,7 @@ class Appr(Inc_Learning_Appr):
                             type=int,
                             default=64)
         parser.add_argument('--alpha',
-                            help='Weight of pizdolos loss',
+                            help='Weight of singularity loss',
                             type=float,
                             default=1e-4)
         parser.add_argument('--lamb',
@@ -213,6 +213,8 @@ class Appr(Inc_Learning_Appr):
         criterion = self.criterion(num_classes_in_t, self.S, self.device, smoothing=self.smoothing)
         if t == 0 and self.is_rotation:
             criterion = self.criterion(4*num_classes_in_t, self.S, self.device, smoothing=self.smoothing)
+            trn_loader = torch.utils.data.DataLoader(trn_loader.dataset, batch_size=trn_loader.batch_size // 4, num_workers=trn_loader.num_workers, shuffle=True)
+            val_loader = torch.utils.data.DataLoader(val_loader.dataset, batch_size=val_loader.batch_size // 4, num_workers=val_loader.num_workers, shuffle=False)
         self.heads.eval()
         old_heads = copy.deepcopy(self.heads)
         parameters = list(self.model.parameters()) + list(criterion.parameters()) + list(distiller.parameters()) + list(self.heads.parameters())
@@ -220,7 +222,7 @@ class Appr(Inc_Learning_Appr):
 
         for epoch in range(self.nepochs):
             train_loss, train_kd_loss, valid_loss, valid_kd_loss = [], [], [], []
-            train_pizdolos = []
+            train_singularity = []
             train_hits, val_hits = 0, 0
             self.model.train()
             criterion.train()
@@ -244,15 +246,15 @@ class Appr(Inc_Learning_Appr):
                 else:  # feature
                     total_loss, kd_loss = self.distill_features(t, loss, features, images)
 
-                pizdolos = loss_pizdolos(features)
-                total_loss += self.alpha * pizdolos
+                singularity = loss_singularity(features)
+                total_loss += self.alpha * singularity
                 total_loss.backward()
                 torch.nn.utils.clip_grad_norm_(parameters, 1)
                 optimizer.step()
                 if logits is not None:
                     train_hits += float(torch.sum((torch.argmax(logits, dim=1) == targets)))
                 train_loss.append(float(bsz * loss))
-                train_pizdolos.append(float(pizdolos))
+                train_singularity.append(float(singularity))
                 train_kd_loss.append(float(bsz * kd_loss))
             lr_scheduler.step()
 
@@ -284,11 +286,11 @@ class Appr(Inc_Learning_Appr):
             train_kd_loss = sum(train_kd_loss) / len(trn_loader.dataset)
             valid_loss = sum(valid_loss) / len(val_loader.dataset)
             valid_kd_loss = sum(valid_kd_loss) / len(val_loader.dataset)
-            train_pizdolos = sum(train_pizdolos) / len(train_pizdolos)
+            train_singularity = sum(train_singularity) / len(train_singularity)
             train_acc = train_hits / len(trn_loader.dataset)
             val_acc = val_hits / len(val_loader.dataset)
 
-            print(f"Epoch: {epoch} Train: {train_loss:.2f} KD: {train_kd_loss:.3f} Acc: {100 * train_acc:.2f} Singularity: {train_pizdolos:.3f} "
+            print(f"Epoch: {epoch} Train: {train_loss:.2f} KD: {train_kd_loss:.3f} Acc: {100 * train_acc:.2f} Singularity: {train_singularity:.3f} "
                   f"Val: {valid_loss:.2f} KD: {valid_kd_loss:.3f} Acc: {100 * val_acc:.2f}")
 
         if self.distillation == "logit":
@@ -681,7 +683,7 @@ def compute_rotations(images, targets, total_classes):
     return images, targets
 
 
-def loss_pizdolos(features):
+def loss_singularity(features):
     # Idea 1 - determinant
     cov = torch.cov(features.T)
     det = torch.det(cov)