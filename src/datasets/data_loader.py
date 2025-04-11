819239d5999eef6db2643483e4135285ab7f7a39
grypesc <g.rypesc@wp.pl>
2025-04-11 16:43:44 +0200

    revert

--- src/datasets/data_loader.py
+++ src/datasets/data_loader.py
@@ -250,12 +250,11 @@ def get_transforms(resize, test_resize, pad, crop, flip, normalize, extend_chann
         trn_transform_list = [
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
-                ImageNetPolicy(),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                     ]
         tst_transform_list = [
-            transforms.Resize((256, 256)),
+            transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
