ae8fbd8a596fc2ea0a82b9dc0489fcc72d78ea94
grypesc <g.rypesc@wp.pl>
2025-04-14 21:25:09 +0200

    ok

--- scripts/10x10-cifar.sh
+++ scripts/10x10-cifar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for SEED in 0
+for SEED in 1
 do
   for VAL in 10
   do
-    CUDA_VISIBLE_DEVICES=0 python src/main_incremental.py --approach camp_cov --seed $SEED --batch-size 128 --num-workers 4 --nepochs 200 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.1 --weight-decay 5e-4 --adaptation-strategy full --S 64 --lamb 10 --use-test-as-val --criterion ce --distillation projected  --rotation --mahalanobis --normalize --multiplier 32  mlp --adapter mlp --nnet resnet18 --exp-name v1/cov_
+    CUDA_VISIBLE_DEVICES=0 python src/main_incremental.py --approach camp_cov --seed $SEED --batch-size 128 --num-workers 4 --nepochs 200 --datasets cifar100_icarl --num-tasks 10 --nc-first-task 10 --lr 0.1 --weight-decay 5e-4 --adaptation-strategy full --S 64 --lamb 10 --use-test-as-val --criterion ce --distillation projected  --rotation --mahalanobis --normalize --multiplier 32  --distiller mlp --adapter mlp --nnet resnet18 --exp-name v1/cov_
   done
 done
