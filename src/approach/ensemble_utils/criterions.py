f88a25fdfca4d184b4e3cc5d237dde4f45099a96
Grzegorz Rypeść <g.rypesc@wp.pl>
2025-09-25 13:42:27 +0200

    SCE works

--- src/approach/ensemble_utils/criterions.py
+++ src/approach/ensemble_utils/criterions.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 import torch.nn.functional as F
 
@@ -36,18 +38,25 @@ class SCE(torch.nn.Module):
         self.heads = torch.nn.ModuleList([torch.nn.Linear(sz_embedding, nb_classes, device=device) for _ in range(self.num_experts)])
         self.smoothing = smoothing
         self.nb_classes = nb_classes
-        self.smooth_matrices = torch.diagonal((num_experts, nb_classes, nb_classes), device=device)
+        # smoothing_matrices is a symmetrical matrix of probabilities for each class. Its columns sum to 1
+        self.smooth_matrices = torch.zeros((num_experts, nb_classes, nb_classes), device=device)
         for e in range(num_experts):
-            for class_ in range(nb_classes):
-                indices = [0, 3, 5]
-                for index in indices:
-                    if self.smooth_matrices[num_experts, ]
-
+            indices_to_smooth = torch.randint(0, nb_classes, (10, 2), device=device)
+            indices_to_smooth = indices_to_smooth[indices_to_smooth[:, 0] != indices_to_smooth[:, 1]]
+            symmetrical_indices = indices_to_smooth.flip(1)
+            indices_to_smooth = torch.cat((indices_to_smooth, symmetrical_indices), dim=0)
+            self.smooth_matrices[e, indices_to_smooth[:, 0], indices_to_smooth[:, 1]] = smoothing
+            col_sum = self.smooth_matrices[e].sum(1)
+            torch.diagonal(self.smooth_matrices[e])[:] = 1 - col_sum
 
     def forward(self, features, T):
         total_loss = 0
+        bsz = features.shape[0]
+
         for e in range(self.num_experts):
-            target = torch.nn.functional.one_hot(T, num_classes=self.nb_classes)
+            sm = self.smooth_matrices[e].expand(bsz, self.nb_classes, self.nb_classes)
+            T = T.unsqueeze(1).unsqueeze(1).repeat(1, 1, self.nb_classes)
+            target = torch.gather(sm, 1, T).squeeze(1)
             logits = self.heads[e](features[:, e])
             total_loss += F.cross_entropy(logits, target)
         return total_loss / self.num_experts
